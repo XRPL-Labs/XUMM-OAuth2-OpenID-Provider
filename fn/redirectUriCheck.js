b8068cb22755cc9d17261b2737719b8c58f33640
Wietse Wind <mail@wietse.com>
2022-06-09 11:33:02 +0200

    Sanitize URL, strip params & hash when comparing

--- fn/redirectUriCheck.js
+++ fn/redirectUriCheck.js
@@ -1,13 +1,22 @@
-module.exports = function redirectUriCheck (dbresult, uri) {
+const sanitizeRedirectUri = url => {
+  return url
+    .replace(/^([a-z0-9]+:\/\/[^?#]+)[?#]*.*/, '$1')
+    .replace(/\/+$/, '')
+}
+
+module.exports = function redirectUriCheck(dbresult, uri) {
   if (Array.isArray(dbresult) && dbresult.length > 0) {
     if (Array.isArray(dbresult[0]) && dbresult[0].length > 0) {
       if (dbresult[0][0]?.['redirect-url']) {
         const uris = dbresult[0][0]?.['redirect-url'].trim().split("\n")
-          .filter(u => u.match(/^[a-z]{1,}[a-z0-9]{1,}:\//))
+          .filter(u => u.match(/^[a-z0-9]{1,}[a-z0-9]{1,}:\//))
+          .map(u => sanitizeRedirectUri(u))
         
-        console.log({uris, uri})
+        const sUri = sanitizeRedirectUri(uri)
+        
+        console.log({uris, uri, sUri})
 
-        if (uris.filter(u => u === uri).length > 0) {
+        if (uris.filter(u => u === sUri).length > 0) {
           return dbresult
         }
       }
