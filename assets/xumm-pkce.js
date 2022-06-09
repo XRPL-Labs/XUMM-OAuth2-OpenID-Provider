184ff6ec4cb671494ab099a1719d2ea757aad35f
Wietse Wind <mail@wietse.com>
2022-06-09 11:37:56 +0200

    Redirect indication on PKCE

--- assets/xumm-pkce.js
+++ assets/xumm-pkce.js
@@ -1,6 +1,9 @@
 var pkce_options = document.getElementById('pkce_options')
 var target_uri = document.querySelector('script[target_uri]').getAttribute('target_uri')
 
+var redirecting = document.getElementById('redirecting')
+var redirected = document.getElementById('redirected')
+
 if (pkce_options) {
   try {
     var options = JSON.parse(decodeURIComponent(pkce_options.innerText))
@@ -27,7 +30,16 @@ if (pkce_options) {
     } else {
       console.log('Origin window gone?', options.full_redirect_uri)
       if (typeof options.full_redirect_uri === 'string') {
-        document.location.href = options.full_redirect_uri
+        if (redirecting) {
+          redirecting.style.display = 'none'
+        }
+        if (redirected) {
+          redirected.setAttribute('style', '')
+          redirected.style.display = 'block'
+        }
+        setTimeout(function () {
+          document.location.href = options.full_redirect_uri
+        }, 1)
       }
     }
   } catch (e) {
--- fn/renderPkceRedirect.js
+++ fn/renderPkceRedirect.js
@@ -7,10 +7,12 @@ module.exports = async function renderPkceRedirect (req, res, options) {
     <!-- Return URL: ${options?.redirect_uri} -->
 
     <textarea id="pkce_options" style="display: none;">${encodeURIComponent(JSON.stringify(options))}</textarea>
-    <script target_uri="${options?.redirect_uri}" src="/assets/xumm-pkce.js"></script>
 
-    <pre>Redirecting...</pre>
-  `.trim()
+    <pre id="redirecting">Redirecting to Xumm...</pre>
+    <pre id="redirected" style="display: none;">Redirected to Xumm...</pre>
+
+    <script target_uri="${options?.redirect_uri}" src="/assets/xumm-pkce.js"></script>
+    `.trim()
 
   res.status(200).send(html)
 }
