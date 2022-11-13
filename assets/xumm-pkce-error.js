0cf319af697c2760de674e6f635431b5bcd59660
Wietse Wind <mail@wietse.com>
2022-11-13 22:22:58 +0100

    Cleanup comment

--- assets/xumm-pkce-error.js
+++ assets/xumm-pkce-error.js
@@ -1,9 +1,5 @@
 if (window.opener) {
-  window.onbeforeunload = function () {
-    // Nope, not required, window close is expected
-    // console.log('Before Unload (error)')
-    // window.opener.postMessage(JSON.stringify({
-    //   source: 'xumm_sign_request_popup_closed',
-    // }), '*')
-  }
+  // window.opener.postMessage(JSON.stringify({
+  //   source: 'xxx',
+  // }), '*')
 }
